f277623cea31e882b0e1de29bb8c04da6c5c2413
soukaina203 <soukaina9mourabit@gmail.com>
2023-07-10 03:07:07 +0000

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    
+   <div>
+hello f
+   </div>
+  )
+}
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,8 @@
+import mongoose from'mongoose'
+export async function connect(){
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
